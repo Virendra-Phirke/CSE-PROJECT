c234d0a6484255e91d60a7eedf93315634e53e29
virendra  phirke <114152472+Virendra-Phirke@users.noreply.github.com>
2025-12-08 11:45:18 +0530

    fix: Use different base URL for dev vs production

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/CSE_FINAL_YEAR_PROJECT/',
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/CSE_FINAL_YEAR_PROJECT/' : '/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
+}));
